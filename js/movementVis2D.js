ae3a67d298005d393c4bb5e0b139184fe6b21d45
Yuhang <gu_yuhang@outlook.com>
2017-08-25 00:37:47 +0200

    1

--- js/movementVis2D.js
+++ js/movementVis2D.js
@@ -244,8 +244,7 @@ function initializeFlowmap(callback1,callback2) {
     Vis.g_basemap2D = Vis.svgFlows.append("g").attr("class", "basemap");
 
     Vis.g_flows2D = Vis.svgFlows.append("g");
-
-
+    
     // set the gradient color
     Vis.g_gradient = Vis.g_flows2D.append("defs")
         .append("linearGradient")
